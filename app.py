09e0139316e81b625b5839b5a0b528de9a129de0
nzubemush <nzubechukwu.okoye@gmail.com>
2022-08-07 10:19:39 +0100

    Create a dummy ToDo App

--- app.py
+++ app.py
@@ -4,15 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 import sys
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://moshe:Billgates2.0@localhost:5432/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://moshe:Billgates2.0@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
 
+@app.route('/')
+def index():
+    return render_template('index.html', data=[{
+        'description': 'Todo 1'
+    }, {
+        'description': 'Todo 2'
+    }, {
+        'description': 'Todo 3'
+    }, {
+        'description': 'Todo 4'
+    }])
 
 
-
-
-#always include this at the bottom of your code
+# always include this at the bottom of your code
 if __name__ == '__main__':
     app.debug = True
     app.run(host="127.0.0.1", port=3000)
